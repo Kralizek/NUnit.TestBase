b5e9b5975580549d181e4b3e2c5ef0ad462ebb6b
Renato Golia <kralizek@gmail.com>
2015-09-24 22:15:56 +0200

    Created project

---
+++ src/TestBase.NUnit/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace TestBase
+{
+    [TestFixture]
+    public abstract class TestBase
+    {
+        protected IFixture Fixture;
+
+        [SetUp]
+        public void InitializeBase()
+        {
+            Fixture = new Fixture();
+            CustomizeFixture(Fixture);
+        }
+
+        protected virtual void CustomizeFixture(IFixture fixture)
+        {
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        }
+
+        [TestFixtureSetUp]
+        protected virtual void SetUpFixture() { }
+
+        [TestFixtureTearDown]
+        protected virtual void TearDownFixture() { }
+    }
+
+    public abstract class TestBase<T> : TestBase
+    {
+        protected abstract T CreateSystemUnderTest();
+    }
+}
